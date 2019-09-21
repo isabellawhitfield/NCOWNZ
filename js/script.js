2f1f1169ec0ecfdebe80800cfe986dcaacce2b9c
isabellawhitfield <isabellawhitfield@gmail.com>
2019-09-21 17:41:43 +1200

    Add menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-    $(".infographics").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true
-      });
+$(document).ready(function () {
+  $(".infographics").slick({
+    lazyLoad: 'ondemand', // ondemand progressive anticipated
+    infinite: true
   });
-  
+
+  $('.header__menu').click(function() {
+    $('.menu').toggleClass('is-open');
+  });
+
+  $('.menu__close-bar').click(function() {
+    $('.menu').toggleClass('is-open');
+  });
+});
